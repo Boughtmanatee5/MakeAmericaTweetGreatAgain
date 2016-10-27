7063b1b496e556c56b7e0c8f1d5c4eddde76edea
Sean Costello <seancostellodp@gmail.com>
2016-10-27 14:54:00 -0400

    initial commit

---
+++ tweetGenerator.py
@@ -0,0 +1,19 @@
+
+class TweetGenerator:
+    def __init__():
+        pass
+
+    def loadTweets(savedTweets):
+        pass
+
+    def processTweets():
+        pass
+
+    def loadTweetGen():
+        pass
+
+    def saveTweetGen():
+        pass
+
+    def generateTweet():
+        pass 
